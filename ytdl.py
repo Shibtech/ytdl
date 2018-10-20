99d52b724ea8dd44903c40c3971288d0eb3e617b
Shibtech <shib@mail.shibtech.net>
2018-10-20 17:21:14 +0900

    key delete

--- ytdl.py
+++ ytdl.py
@@ -15,7 +15,7 @@ que_url = queue.Queue()
 # tab of
 #   https://cloud.google.com/console
 # Please ensure that you have enabled the YouTube Data API for your project.
-DEVELOPER_KEY = "AIzaSyCBFwZ67qxzaA-Sd6vTZaYshyLEW9RXiqg"
+DEVELOPER_KEY = "Your Develeper Key"
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 YOUTUBE_BASEURL = "https://www.youtube.com/watch?v="
